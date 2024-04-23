a8335a190fea138f9e11a637ef97ed9c41b1d6e5
flores-ac <manuel@floresac.info>
2024-04-23 01:27:05 -0600

    typo fix

--- Routes/Chat/LangchainRetrival.js
+++ Routes/Chat/LangchainRetrival.js
@@ -301,7 +301,7 @@ const langchainRetrival = async (Email, ChatId, Question) => {
         return result.userInstructions + "\n\n" + result.viewQuery + "\n\n" + result.purchaseQuery;
       } catch (error) {
         console.error("Error occurred while processing the prompt response:", error);
-        return "Error occurred while processing the prompt response:", error;
+        return "Error occurred while processing the prompt response";
       }
     } else if (classificationChainResult == "notQuery") {
       // Extrating data from Vector Data bases
