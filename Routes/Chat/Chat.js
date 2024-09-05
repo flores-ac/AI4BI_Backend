ba3157cf1c01188c9fc00c51db20b38730456ac2
flores-ac <manuel@floresac.info>
2024-09-05 17:43:20 -0600

    Re-implemented new logic for langchain

--- Routes/Chat/Chat.js
+++ Routes/Chat/Chat.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const chatModel = require("../../Schema/chatModel");
-const LangchainRetrival = require("./LangchainRetrival");
+const LangchainRetrival = require("./LangchainRetrivalOld");
 
 const router = express.Router();
 
